73caff52908f908339f48a326fc6b3fc8dfcfd01
Sujeet <sujeetkgoyal@gmail.com>
2020-02-22 23:31:21 +0530

    'changes'

--- ForJob/src/app/app.module.ts
+++ ForJob/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+import { AppRoutingModule, routingComponent } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ LoginComponent ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ ForJob/src/app/classes/auth.ts
@@ -0,0 +1,7 @@
+export class auth{
+    id: number;
+    firstname: string;
+    lastname: string;
+    username: string;
+    password: string;
+}
